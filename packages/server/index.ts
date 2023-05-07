06dc5e21b2591cc69b3cadc18d7027d4e3873431
Kriksy <viktoria.toldi@gmail.com>
2023-05-07 12:20:13 +0200

    [server] Update index with database connection

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,8 +1,23 @@
 import { server } from "./src/server";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const port = process.env.PORT || 9000;
+
+// Setup Database
+const mongoURL: string =
+  process.env.MONGODB_URL || "mongodb://localhost:27017/snoutbook";
+
+mongoose.connect(mongoURL).then(function (v) {
+  if (mongoose.connection.readyState === 1) {
+    console.log(`"[database]: connected to ${mongoURL}"`);
+  } else {
+    console.log("[database]: database connection error");
+  }
+});
 
 // Server Start
 server.listen(port, () => {
-  console.log(`[server]: Server is running at https://localhost:${port}`);
+  console.log(`[server]: http server is running at https://localhost:${port}`);
 });
