987ef60487b1128f407e38fab8a8f1d1b17cbe2d
Kriksy <viktoria.toldi@gmail.com>
2023-05-18 09:08:05 +0200

    [client] Add LoginPage

---
+++ packages/client/src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import LoginForm from '../../account/LoginForm'
+import { AppContainer } from "../../components/AppContainer"
+
+
+export default function LoginPage() {
+    return (
+        <div>
+            <AppContainer>
+                <LoginForm />
+            </AppContainer>
+        </div>
+    )
+}
+
