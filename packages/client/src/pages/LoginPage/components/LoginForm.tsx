1a694164536956465d218519da1064ea8a4d4680
Kriksy <viktoria.toldi@gmail.com>
2023-05-18 09:22:51 +0200

    [client] Move LoginForm to LoginPage/components

--- packages/client/src/pages/LoginPage/components/LoginForm.tsx
+++ packages/client/src/pages/LoginPage/components/LoginForm.tsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Form, FormProps, Button, Segment } from 'semantic-ui-react'
-import { authenticateUser } from "../shared"
+import { Form, FormProps, Button, Segment, Grid, Message, Header, Image } from 'semantic-ui-react'
+import { authenticateUser } from "../../../shared"
 
 export default function LoginForm() {
     const [errorMessage, setErrorMessage] = useState("")
-    const navigate = useNavigate()
+
+    const navigate = useNavigate();
+
+    const [state] = useState({ activeItem: 'home' })
+
+    const handleItemClick = (e: any, { name }: any) => (
+        navigate("/" + name)
+    )
+
+    const { activeItem } = state
 
     function onSubmit(e: React.FormEvent<HTMLFormElement>, data: FormProps) {
         e.preventDefault()
@@ -40,8 +49,26 @@ export default function LoginForm() {
                     <label>Password</label>
                     <input name="password" placeholder='Password' />
                 </Form.Field>
-                <Button type='submit'>Submit</Button>
+                <Grid columns={2} divided inverted>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Button
+                                className="text-align-left"
+                                type='submit'
+                                name='create_user'
+                                active={activeItem === 'create_user'}
+                                onClick={handleItemClick}> Sign-in </Button>
+                        </Grid.Column>
+                        <Grid.Column className="text-align-right" >
+                            <Button
+                                color='blue'
+                                type='submit'> Log-in
+                            </Button>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
             </Form>
         </Segment >
     )
 }
+
