152cd995651d8d8378752523aae4cec86c550570
Kriksy <viktoria.toldi@gmail.com>
2023-05-27 10:07:04 +0200

    [server] Update log

--- packages/server/setup_db.ts
+++ packages/server/setup_db.ts
@@ -7,25 +7,28 @@ const port = process.env.SERVER_PORT || 9000;
 
 // Setup Database
 const mongoURL: string =
-    process.env.MONGODB_URL || "mongodb://localhost:27017/snoutbook";
+  process.env.MONGODB_URL || "mongodb://localhost:27017/snoutbook";
 
 mongoose.connect(mongoURL).then(function (v) {
-    if (mongoose.connection.readyState === 1) {
-        console.log(`[database]: connected to ${mongoURL}`);
-    } else {
-        console.log("[database]: database connection error");
-        return;
-    }
+  if (mongoose.connection.readyState === 1) {
+    console.log(`[database]: connected to ${mongoURL}`);
+  } else {
+    console.log("[database]: database connection error");
+    return;
+  }
 
-    createUser({ username: "admin", password: "admin", role: "admin" })
-        .then(() => {
-            console.log("admin user created")
-        })
-        .catch(
-            (error) => {
-                console.log(error.message)
-                console.log("admin user already created")
-            }
-        ).finally(() => process.exit(0));
+  createUser({ username: "admin", password: "admin", role: "admin" })
+    .then(() => {
+      console.log("[setup_db.ts]: admin user created");
+    })
+    .catch((error) => {
+      if (
+        error.message === "User validation failed: username: is already taken"
+      ) {
+        console.log("[setup_db.ts]: admin user already exists");
+      } else {
+        console.log("[setup_db.ts]: " + error.message);
+      }
+    })
+    .finally(() => process.exit(0));
 });
-
